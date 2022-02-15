baf5ee68f358bd971025bf9232feae9c16cf7ce7
Daniel Dixon <github@thedixons.com>
2022-02-14 21:37:13 -0700

    get_message added and some other generalizing

--- src/ble_web_json_comm.cpp
+++ src/ble_web_json_comm.cpp
@@ -1,22 +1,12 @@
 #include "ble_web_json_comm.h"
 
-#if PLATFORM_ID != 12 // Argon
-    #error "This library only works on the Argon!"
-#else
-
-Logger BLEwebJsonCommLog("app.BLEwebJsonCommLog");
+Logger bleLog("app.bleLog");
 
 static void onDataReceived(const uint8_t* rx_data, size_t len, const BlePeerDevice& peer, void* self) {
     // Is this really the way to do this?
-    Log.info("BLE MANAGER: in onDataReceived routing");
+    //bleLog.info("in onDataReceived routing with %s", rx_data);
     ((BLEWebJsonComm*)self)->queue_msg(rx_data, len);
 }
-
-BLEWebJsonComm::BLEWebJsonComm() 
-  : config_state(STATE_CONFIG_SETUP),
-    next_config_state(STATE_CONFIG_SETUP),
-    provisionCb(nullptr)
-{}
 
 void BLEWebJsonComm::setup() {
     rxCharacteristic = new BleCharacteristic("rx", BleCharacteristicProperty::NOTIFY, readUUID, serviceUUID);
@@ -25,61 +15,51 @@ void BLEWebJsonComm::setup() {
     BLE.addCharacteristic(*rxCharacteristic);
     BLE.addCharacteristic(*txCharacteristic);
 
-    // Advertise our custom configuration service UUID so the webapp can detect compatible devices
-    BleAdvertisingData advData;
+    BleAdvertisingData advData; // Advertise service UUID so webapp can detect
     advData.appendServiceUUID(serviceUUID);
     BLE.advertise(&advData);
 
-    BLEwebJsonCommLog.trace("Bluetooth Address: %s", BLE.address().toString().c_str());
+    bleLog.trace("Setup:  Bluetooth Address: %s", BLE.address().toString().c_str());
     BLE.on();
 
-    // WiFi must be on for this library to work
-    //WiFi.on();
 }
 
-void BLEWebJsonComm::loop() {
-
-    char tmp_buf[150];  // Need: ~64 chars + SSID length + null terminator
-
-    int len = sprintf(tmp_buf, "{\"msg_t\":\"general_debug\", \"key1\":\"%s\", \"key2\":\"%s\", \"key3\":%d}", 
-				"value1", "value2", 3 );
-
-    BLEwebJsonCommLog.trace("Sending message from device to browser %s",tmp_buf);
+void BLEWebJsonComm::sendMessage(char* message) {
+    char tmp_buf[1024];
+    int len = sprintf(tmp_buf, "%s", message );
+    bleLog.trace("sendMessage:  Sending message from device to browser %s",tmp_buf);
     rxCharacteristic->setValue((uint8_t*)tmp_buf, len);  // message from device to browser
 }
 
-void BLEWebJsonComm::parse_message() {
-    // Pull our message off the queue, copy it locally, and free the original message
-    // Probbaly not ideal since we don't really need to copy, but since we parse and have conditional
-    // actions later, this may prevent accidental memory leaks with the addition of other conditional paths in this code 
-    // e.g. returning somewhere down below before the free() call.
-    char *msg_buf = device_receive_msg_queue.front();
-    BLEwebJsonCommLog.trace("String RX: %s", msg_buf);
-    JSONValue outerObj = JSONValue::parseCopy(msg_buf);
-    device_receive_msg_queue.pop();
-    free(msg_buf);
+int BLEWebJsonComm::get_message( char* msgBuf, size_t bufSize ) {
+    bleLog.trace("get message called");
+    bleLog.trace("queue size: %d", device_receive_msg_queue.size() );
 
-    // Process our received message
-    JSONObjectIterator iter(outerObj);
-    while(iter.next()) {
-        BLEwebJsonCommLog.info("key=%s value=%s", 
-            (const char *) iter.name(), 
-            (const char *) iter.value().toString());
+    if ( device_receive_msg_queue.empty() ) {
+        msgBuf[0] = '\0';
+	return 0;
     }
+
+    strncpy( msgBuf, device_receive_msg_queue.front(), bufSize );
+    char *memHog = device_receive_msg_queue.front();
+    device_receive_msg_queue.pop();
+    free( memHog);
+    return 1;
 }
 
 void BLEWebJsonComm::queue_msg(const uint8_t* rx_data, size_t len) {
+        //bleLog.trace("queue message: %s", rx_data);
     if( len > 0 ) {
-        // The underlying BLE lib reuses the receive buffer, and will not terminate it properly for a string
-        // Add some manual processing and properly terminate for string parsing
+        // The underlying BLE lib reuses the receive buffer, 
+        // and will not terminate it properly for a string
+        // Adding manual processing, properly terminating string
         char *msg_buf = (char*)malloc(len+1);
         memcpy(msg_buf, rx_data, len);
         msg_buf[len] = 0;   // Null-terminate string
         device_receive_msg_queue.push(msg_buf);
         Log.info("BLE MANAGER:  Added message to the queue: %s", msg_buf);
-        BLEwebJsonCommLog.trace("Added message to the queue: %s", msg_buf);
+        bleLog.trace("queued message: %s", msg_buf);
+        bleLog.trace("queue size: %d", device_receive_msg_queue.size() );
         return;
     }
 }
-
-#endif  // PLATFORM_ID == 12
--- src/ble_web_json_comm.h
+++ src/ble_web_json_comm.h
@@ -3,39 +3,13 @@
 #include <queue>
 
 class BLEWebJsonComm {
-
-    typedef enum {
-        STATE_CONFIG_SETUP = 0,
-        STATE_CONFIG_IDLE,
-        STATE_CONFIG_PARSE_MSG,
-    } ConfigState_t;
-
-    typedef void (provisionCb_t)(void);
-
     public:
-        BLEWebJsonComm();
-
         void setup();
-        void loop();
-
-        void enable();
-
-        void wifi_scan_handler(WiFiAccessPoint* wap);
+	void sendMessage(char* message);
         void queue_msg(const uint8_t* rx_data, size_t len);
-
-        void setProvisionCallback(provisionCb_t* cb);
-
+        int get_message( char* msgBuf, size_t bufSize );
     private:
-        ConfigState_t config_state;
-        ConfigState_t next_config_state;
-
-        provisionCb_t *provisionCb;
-
-        void parse_message();
-
-        std::queue<WiFiAccessPoint> wifi_scan_response_queue;
         std::queue<char*> device_receive_msg_queue;
-
         BleCharacteristic *rxCharacteristic;
         BleCharacteristic *txCharacteristic;
 };
